028dfd68207132410e8e5a4371b554c47bd3692d
PAULO ARRUDA <paulo.arruda@masterboi.com.br>
2025-02-04 18:26:29 -0300

    Aula Lista dos times

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,27 @@
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import './Formulario.css'
+
+const Formulario = () => {
+    const times =[
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'DevOps',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+    return (
+        <section className="formulario">
+            <form>
+                <h2>Preencha os dados para criar o card do colaborador.</h2>
+                <CampoTexto label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da sua imagem" />
+                <ListaSuspensa label="Time" itens={times}/>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
